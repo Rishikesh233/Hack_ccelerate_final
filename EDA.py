ceeefcc17c7a8a50f02c1d7698b4946ba45630bb
Rishikesh Maurya <79248552+Rishikesh233@users.noreply.github.com>
2023-03-04 23:20:21 +0530

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -64,8 +64,8 @@ with col1:
 
 if ((button3 != True) & (button5 != True)):
     col1, col2 = st.columns([1, 4])
-    logo = Image.open('WebsiteLogo.png')
-    col1.image(logo, width=200)
+#     logo = Image.open('WebsiteLogo.png')
+#     col1.image(logo, width=200)
     engage = Image.open('image001.jpg')
     col2.image(engage, width=900)
 
